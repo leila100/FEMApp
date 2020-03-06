cb542f27932313ffe2e429cd5fc9beec57da3511
Leila <nb.leila10@gmail.com>
2020-03-06 11:34:11 -0600

    Solve styling exercise

--- App.js
+++ App.js
@@ -1,10 +1,21 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.box}>
+        <Text style={{ backgroundColor: "#2aa198", ...styles.text }}>Cyan: #2aa198</Text>
+      </View>
+      <View style={styles.box}>
+        <Text style={{ backgroundColor: "#268bd2", ...styles.text }}>Blue: #268bd2</Text>
+      </View>
+      <View style={styles.box}>
+        <Text style={{ backgroundColor: "#d33682", ...styles.text }}>Magenta: #d33682</Text>
+      </View>
+      <View style={styles.box}>
+        <Text style={{ backgroundColor: "#cb4b16", ...styles.text }}>Orange: #cb4b16</Text>
+      </View>
     </View>
   );
 }
@@ -12,8 +23,18 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
   },
+  box: {
+    width: "100%",
+    padding: 5,
+    color: "white"
+  },
+  text: {
+    textAlign: "center",
+    padding: 10,
+    color: "white"
+  }
 });
